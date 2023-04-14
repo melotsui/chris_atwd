e43ea84c9ffc02980d4b659e08a261fe1131e3d7
melo <qonkgonk@gmail.com>
2023-04-14 09:57:17 +0800

    Access-Control-Allow

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 header('Content-Type: application/json; charset=utf-8');
+
  class Controller {
     private $pathInfo;
     private $urlSegments;
